01797bce76fc49f8965cf7b75613a2b9371060f7
NRotondaro <nikson_rotondaro@hotmail.com>
2021-10-05 15:44:42 -0300

    feat(tracks): using queries

--- client/src/index.js
+++ client/src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import GlobalStyles from './styles';
 import Pages from './pages';
-import { ApolloProvider, ApolloClient, InMermoryCache } from '@apollo/client';
+
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
 
 const client = new ApolloClient({
   uri: 'http://localhost:4000',
-  cache: new InMermoryCache(),
+  cache: new InMemoryCache(),
 });
 
 ReactDOM.render(
