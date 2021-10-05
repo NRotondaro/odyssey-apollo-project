39ac8b80ee94ed8e40441f445496c6e63537c90a
NRotondaro <nikson_rotondaro@hotmail.com>
2021-10-05 15:13:25 -0300

    feat(client): added apollo-provider

--- client/src/index.js
+++ client/src/index.js
@@ -2,11 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import GlobalStyles from './styles';
 import Pages from './pages';
+import { ApolloProvider, ApolloClient, InMermoryCache } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000',
+  cache: new InMermoryCache(),
+});
 
 ReactDOM.render(
-  <React.StrictMode>
+  <ApolloProvider client={client}>
     <GlobalStyles />
     <Pages />
-  </React.StrictMode>,
+  </ApolloProvider>,
   document.getElementById('root')
 );
